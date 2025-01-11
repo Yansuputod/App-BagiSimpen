a759e6aad79aac20eaba69921843e054907d0f40
Yansuputod <arta70660@gmail.com>
2025-01-12 03:30:09 +0800

    fixing commit

--- lib/presentation/util/qr_scanner.dart
+++ lib/presentation/util/qr_scanner.dart
@@ -5,6 +5,7 @@ class QRScanner extends StatefulWidget {
   const QRScanner({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _QRScannerState createState() => _QRScannerState();
 }
 
