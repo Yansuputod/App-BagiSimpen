6c30b53a386cd6bd370bc5e76441a29eeefde9b3
Yansuputod <arta70660@gmail.com>
2025-01-12 03:10:16 +0800

    add payment bar dan qr scanner

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:bagisimpen/presentation/pages/search.dart';
+import 'package:bagisimpen/presentation/util/qr_scanner.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,13 +13,11 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(240, 240, 240, 1),
       appBar: searchBar(context),
-      body: const Center(
-        child: Text(
-          "Ini blom lese cokkk",
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            eMoney(context),
+          ],
         ),
       ),
     );
@@ -52,7 +54,7 @@ class Home extends StatelessWidget {
                     Text(
                       'Search...',
                       style: TextStyle(
-                          color: Color.fromRGBO(0, 99, 13, 1.0), fontSize: 18),
+                          color: Colors.grey, fontSize: 18),
                     ),
                     Spacer(),
                     Padding(
@@ -74,6 +76,138 @@ class Home extends StatelessWidget {
             icon: const Icon(Icons.message),
             color: Colors.white,
             onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  SizedBox eMoney(BuildContext context) {
+    return SizedBox(
+      height: 65,
+      child: Stack(
+        children: [
+          Container(
+            height: 30,
+            color: const Color.fromRGBO(0, 99, 13, 1.0),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 60,
+              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: SvgPicture.asset("assets/svg/scan.svg"),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const QRScanner()),
+                      );
+                    },
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    child: VerticalDivider(
+                      color: Colors.grey,
+                      thickness: 0.5,
+                      width: 1,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              "assets/svg/wallet.svg",
+                              color: const Color.fromRGBO(0, 99, 13, 1.0),
+                              height: 20,
+                              width: 15,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 5),
+                              child: Text(
+                                "Rp 100.250",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          "Saldo Anda",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    child: VerticalDivider(
+                      color: Colors.grey,
+                      thickness: 0.5,
+                      width: 1,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(bottom: 8, top: 8, right: 10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              "assets/svg/tarik.svg",
+                              color: const Color.fromRGBO(0, 99, 13, 1.0),
+                              height: 20,
+                              width: 15,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 5),
+                              child: Text(
+                                "Tarik Tunai",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Text(
+                          "Gratis",
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
---
+++ lib/presentation/util/qr_scanner.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class QRScanner extends StatefulWidget {
+  const QRScanner({super.key});
+
+  @override
+  _QRScannerState createState() => _QRScannerState();
+}
+
+class _QRScannerState extends State<QRScanner> {
+  String? qrCodeResult;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 90),
+          child: Image.asset(
+            'assets/logo/Logo.png',
+            fit: BoxFit.contain,
+            height: 40,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: MobileScanner(
+              onDetect: (barcodeCapture) {
+                final String code = barcodeCapture.barcodes.first.rawValue ?? "Unknown";
+                setState(() {
+                  qrCodeResult = code;
+                });
+              },
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(color: Colors.grey.shade300, width: 1),
+              ),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.qr_code, size: 30),
+                SizedBox(width: 10),
+                Text(
+                  "Scan Kode QR",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          if (qrCodeResult != null)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'QR Code: $qrCodeResult',
+                style: const TextStyle(fontSize: 18),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
